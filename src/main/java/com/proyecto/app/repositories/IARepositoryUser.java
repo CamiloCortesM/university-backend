11e4898504e9cb63ba66c2c158ac5a10c87b7f60
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-10-04 01:21:00 -0500

    fix errors

--- src/main/java/com/proyecto/app/repositories/IARepositoryUser.java
+++ src/main/java/com/proyecto/app/repositories/IARepositoryUser.java
@@ -1,9 +1,11 @@
 package com.proyecto.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.proyecto.app.entities.User;
 
+@Repository
 public interface IARepositoryUser extends JpaRepository<User, Long> {
 
 	boolean existsByEmail(String emailToCheck);
